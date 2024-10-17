5d02ea6baec7adef41c3373f96c1c6cc082e4b40
Ritika <ritz21ch@gmail.com>
2024-10-17 13:30:33 +0530

    create group chat feature using websocket and inmemorychannellayer

--- ChatApp/asgi.py
+++ ChatApp/asgi.py
@@ -8,17 +8,22 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
 
 import os
-from channels.routing import ProtocolTypeRouter
+from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
-
-
+from chat import routing
+from channels.auth import AuthMiddlewareStack
 
 django_asgi_app = get_asgi_application()
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ChatApp.settings")
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
-    # Just HTTP for now. (We can add other protocols later.)
+    "websocket" : AuthMiddlewareStack(
+            URLRouter(
+                routing.websocket_urlpatterns
+            )    
+        )
+
 })
 
 
--- ChatApp/settings.py
+++ ChatApp/settings.py
@@ -127,3 +127,14 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels.layers.InMemoryChannelLayer"
+    }
+}
+
+LOGIN_REDIRECT_URL = "chat-page"
+LOGOUT_REDIRECT_URL="login-user"
+
+
